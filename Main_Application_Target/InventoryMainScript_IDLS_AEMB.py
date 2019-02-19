c1d9dda64ca80917b27b12714757e5196375b626
Janrey Licas <janjan.licas@gmail.com>
2019-02-19 20:57:32 +0800

    Initial Commit and Starting Point
    Files are being initialized and some parts are copied for main purposes... KivyMD Main Files will be modified...

---
+++ Main_Application_Target/InventoryMainScript_IDLS_AEMB.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.metrics import dp
+from kivy.properties import ObjectProperty
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivymd.bottomsheet import MDListBottomSheet, MDGridBottomSheet
+from kivymd.button import MDIconButton
+from kivymd.date_picker import MDDatePicker
+from kivymd.dialog import MDDialog
+from kivymd.label import MDLabel
+from kivymd.list import ILeftBody, ILeftBodyTouch, IRightBodyTouch, BaseListItem
+from kivymd.material_resources import DEVICE_TYPE
+from kivymd.navigationdrawer import MDNavigationDrawer, NavigationDrawerHeaderBase
+from kivymd.selectioncontrols import MDCheckbox
+from kivymd.snackbar import Snackbar
+from kivymd.theming import ThemeManager
+from kivymd.time_picker import MDTimePicker
